8e6b5c05b0be91855792f2c31afd544870c6979e
AnBinh1703 <dan1314705@gmail.com>
2025-03-07 09:35:16 +0700

    refactor App component to use functional component and hooks for state management

--- src/App.js
+++ src/App.js
@@ -1,22 +1,40 @@
 import React from "react";
 
-class App extends React.Component {
-  state = { count: 0 };
+//version 2
 
-  increment = () => {
-    this.setState({ count: this.state.count + 1 });
-  };
-
-  render() {
-    return (
-      <div>
-        <h1>Counter App</h1>
-        <button onClick={this.increment}>
-          Clicked To Count {this.state.count}
-        </button>
-      </div>
-    );
+const App =() => {
+ const [count, setCount] = React.useState(0);
+  const increment = () => {
+    setCount(count + 1);
   }
+  return (
+    <div>
+        <h1>Counter App</h1>
+         <button onClick={increment}>
+           Clicked To Count {count}
+         </button>
+    </div>
+  );
 }
+
+//version 1
+// class App extends React.Component {
+//   state = { count: 0 };
+
+//   increment = () => {
+//     this.setState({ count: this.state.count + 1 });
+//   };
+
+//   render() {
+//     return (
+//       <div>
+//         <h1>Counter App</h1>
+//         <button onClick={this.increment}>
+//           Clicked To Count {this.state.count}
+//         </button>
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
