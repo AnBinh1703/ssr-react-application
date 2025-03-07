6c0f64276087c2ef177f2342ca5e9c8b8ad2824f
AnBinh1703 <dan1314705@gmail.com>
2025-03-07 09:41:28 +0700

    refactor App component to use class-based state management and update document title on state change

--- src/App.js
+++ src/App.js
@@ -1,21 +1,48 @@
 import React from "react";
 
-//version 2
+//version 3
+class App extends React.Component {
+  state = { count: 0 };
 
-const App =() => {
- const [count, setCount] = React.useState(0);
-  const increment = () => {
-    setCount(count + 1);
+  increment = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+  // no change in the document title
+  componentDidMount() {
+    document.title = `Clicked ${this.state.count} times`;
   }
-  return (
-    <div>
+  // when i click the button the document title will change
+  componentDidUpdate() {
+    document.title = `Clicked ${this.state.count} times`;
+  }
+  render() {
+    return (
+      <div>
         <h1>Counter App</h1>
-         <button onClick={increment}>
-           Clicked To Count {count}
-         </button>
-    </div>
-  );
+        <button onClick={this.increment}>
+          Clicked To Count {this.state.count}
+        </button>
+      </div>
+    );
+  }
 }
+
+// //version 2
+
+// const App =() => {
+//  const [count, setCount] = React.useState(0);
+//   const increment = () => {
+//     setCount(count + 1);
+//   }
+//   return (
+//     <div>
+//         <h1>Counter App</h1>
+//          <button onClick={increment}>
+//            Clicked To Count {count}
+//          </button>
+//     </div>
+//   );
+// }
 
 //version 1
 // class App extends React.Component {
